69783783027288f894aa655a264f3d422ebeddcb
islombek-developer <164884102+islombek-developer@users.noreply.github.com>
2024-07-10 17:44:47 +0300

    sa

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.SET_NULL, null=True)
+    comment = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return self.blog.title
+
+class Like(models.Model):
+    blog = models.OneToOneField(Blog, on_delete=models.CASCADE)
+    like = models.BooleanField()
 
 
